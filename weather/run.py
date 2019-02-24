b01059c5c18a3406655d9900377c036bcda12956
Shadowmaple <1142319190@qq.com>
2019-02-24 15:30:09 +0800

    update

--- weather/run.py
+++ weather/run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 abc = Flask(__name__)
 
 from crawler2 import data
@@ -11,12 +11,21 @@ def index():
 @abc.route('/<city>', methods=['GET', 'POST'])
 def weather(city):
     weather = data(city)
+    if not weather:
+        return "", 500
     state = weather.get('weather')
     city_ch = weather['city_ch']
     tem = weather.get('tem_min')+'~'+weather.get('tem_max') + '摄氏度'
-    return city_ch +" " +state + ' ' + tem
 
-#weather.get('weather')+''+weather.get('tem_min')+'~'+weather.get('tem_max')
+    payload = {
+            'city':city_ch,
+            'weather':state,
+            'temperature':tem
+            }
+    return jsonify(payload), 200
+
+#    return city_ch +" " +state + ' ' + tem
+#    weather.get('weather')+''+weather.get('tem_min')+'~'+weather.get('tem_max')
 
 if __name__ == '__main__':
     abc.run()
